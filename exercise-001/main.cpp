e9702b84ed93e01b526a18fb9a059744ac15dabd
julianpeg <149794361+julianpeg@users.noreply.github.com>
2023-11-03 15:17:08 +0100

    added second data

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -8,9 +8,20 @@
 #include "config.h"
 
 int foo;
+int data = 4711;
+const int rodata = 42;
 
 auto main(int argc, char **argv) -> int
 {
     fmt::print("Hello, {}!\n", "hellooo");
+
+
+
+    fmt::print("value of variable foo {} adaress of variable foo {} \n", foo, fmt::ptr(&foo));
+
+    fmt::print("value of variable foo {} adaress of variable data {} \n", data, fmt::ptr(&data));
+    
+    fmt::print("value of variable foo {} adaress of variable rodata {} \n", rodata, fmt::ptr(&rodata));
+
     return 0; 
 }
