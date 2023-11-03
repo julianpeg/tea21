bbcd37fd29e9c9926ff9e661dbd6ae0f2c8ba8e7
julianpeg <149794361+julianpeg@users.noreply.github.com>
2023-11-03 15:21:55 +0100

    Test 3

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -8,6 +8,7 @@
 #include "config.h"
 
 int foo;
+int var2 = 5;
 int data = 4711;
 const int rodata = 42;
 
@@ -19,9 +20,11 @@ auto main(int argc, char **argv) -> int
 
     fmt::print("value of variable foo {} adaress of variable foo {} \n", foo, fmt::ptr(&foo));
 
-    fmt::print("value of variable foo {} adaress of variable data {} \n", data, fmt::ptr(&data));
+    fmt::print("value of variable var2 {} adaress of variable var2 {} \n", foo, fmt::ptr(&var2));
+
+    fmt::print("value of variable data {} adaress of variable data {} \n", data, fmt::ptr(&data));
     
-    fmt::print("value of variable foo {} adaress of variable rodata {} \n", rodata, fmt::ptr(&rodata));
+    fmt::print("value of variable rodata {} adaress of variable rodata {} \n", rodata, fmt::ptr(&rodata));
 
     return 0; 
 }
